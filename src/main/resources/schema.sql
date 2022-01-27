a2c8e0d8e0668c9854d6f0ffc363f03db6c98592
dimasadriyantos@gmail.com <dimasdz99>
2022-01-27 13:35:07 +0700

    initial commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE employee
     employee_id          bigint PRIMARY KEY,
     employee_name        VARCHAR(150),
     contract_information varchar(150),
-    age                  integer
+    age                  integer,
+    state varchar (100)
 );
